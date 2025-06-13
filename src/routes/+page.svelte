5c8b44787d6715dfac9f373c45c305b6bb8ae7c8
Sebi <sebastian@flick.solutions>
2025-06-13 21:47:35 +0200

    add some logic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import articles from '$lib/articles.json';
+	import Recursive from './Recursive.svelte';
+</script>
+
+<div>
+	<div>
+		List of items
+		<Recursive items={articles} />
+	</div>
+	<div>calculations</div>
+</div>
---
+++ src/routes/Recursive.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Recursive from './Recursive.svelte';
+
+	let { items } = $props();
+</script>
+
+<ul>
+	{#each items as item}
+		<li>
+			{#if item.length > 0}
+				<Recursive items={item} />
+			{:else}
+				<button onclick={() => addItem(1)}>
+					<h2>{item.title}</h2>
+					<p>{item.price}</p>
+				</button>
+			{/if}
+		</li>
+	{/each}
+</ul>
