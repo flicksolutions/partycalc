d26dfdc52504b32e8e2103a8f5fb4149f813014b
flicksolutions <sebastian@flick.solutions>
2025-06-14 10:54:29 +0200

    style calculations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,28 +12,38 @@
 			<Recursive items={articles} />
 		</div>
 	</main>
-	<aside>
-		<h2 class="h2">Bestellung</h2>
-		<div>
-			{#each order.list as item (item.id)}
-				<h2>{item.title}</h2>
-				<p>{item.price}</p>
-				<p>
-					<button class="btn-icon preset-filled" onclick={() => order.reduceOrder(item.id)}
-						>-</button
-					>{item.quantity}<button
-						class="btn-icon preset-filled"
-						onclick={() => order.addOrder(item)}>+</button
-					>
-				</p>
-			{/each}
+	<aside class="grid h-full max-h-full grid-rows-[1fr_auto] overflow-hidden">
+		<div class="overflow-y-auto">
+			<h2 class="h2">Bestellung</h2>
+			<div class="flex max-h-full flex-col gap-2 overflow-y-auto">
+				{#if order.list.length === 0}
+					<p class="text-center">Keine Artikel in der Bestellung</p>
+				{/if}
+				{#each order.list as item (item.id)}
+					<div class="flex items-center justify-between gap-2">
+						<p>{item.title} à {item.price}</p>
+						<p class="shrink-0">
+							<button
+								class="btn-icon btn-icon-lg preset-filled m-2"
+								onclick={() => order.reduceOrder(item.id)}>-</button
+							>{item.quantity}<button
+								class="btn-icon btn-icon-lg preset-filled m-2"
+								onclick={() => order.addOrder(item)}>+</button
+							>
+						</p>
+					</div>
+				{/each}
+			</div>
 		</div>
-		<p>Total: {order.sum}</p>
 		<div>
-			<button class="btn preset-filled-primary-500" onclick={() => order.clearOrders()}>
-				Clear
-			</button>
-			<button class="btn preset-filled-primary-500" disabled>Print</button>
+			<hr class="hr mt-4 mb-2" />
+			<p class="mb-4 text-3xl font-black">Total: {order.sum}</p>
+			<div>
+				<button class="btn preset-filled-primary-500" onclick={() => order.clearOrders()}>
+					Clear
+				</button>
+				<button class="btn preset-filled-primary-500" disabled>Print</button>
+			</div>
 		</div>
 	</aside>
 </div>
