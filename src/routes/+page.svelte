e1fcf6fffb6348d68a13e67fa20676a84bd0c451
flicksolutions <sebastian@flick.solutions>
2025-06-18 18:14:22 +0200

    connect to POS Printer via Bluetooth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,35 @@
 	import articles from '$lib/articles.json';
 	import Recursive from './Recursive.svelte';
 	import { order } from './order.svelte.ts';
+	import { onMount } from 'svelte';
+	import ReceiptPrinterEncoder from '@point-of-sale/receipt-printer-encoder';
+
+	let printer: any;
+	const encoder = new ReceiptPrinterEncoder();
+
+	let printerID = $state();
+	let connected = $state(false);
+
+	onMount(async () => {
+		const WebBluetoothReceiptPrinter = (await import('@receipt-printer')).default;
+		printer = new WebBluetoothReceiptPrinter();
+		printer.addEventListener('connected', (device) => {
+			printerID = device.id;
+			connected = true;
+			console.log('Connected to printer:', device);
+			// encoder = new ReceiptPrinterEncoder(); // might be necessary if codepage or language is not default
+		});
+	});
+
+	const handlePrint = async () => {
+		if (!printerID) {
+			printerID = await printer.connect();
+		} else {
+			await printer.reconnect(printerID);
+		}
+		let data = encoder.initialize().text('Bestellungäöü').newline().encode();
+		printer.print(data);
+	};
 </script>
 
 <div class="grid h-screen gap-5 p-5 md:grid-cols-[4fr_1fr]">
@@ -42,7 +71,7 @@
 				<button class="btn preset-filled-primary-500" onclick={() => order.clearOrders()}>
 					Clear
 				</button>
-				<button class="btn preset-filled-primary-500" disabled>Print</button>
+				<button class="btn preset-filled-primary-500" onclick={handlePrint}>Print</button>
 			</div>
 		</div>
 	</aside>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@receipt-printer': path.resolve(
+				'node_modules/@point-of-sale/webbluetooth-receipt-printer/dist/webbluetooth-receipt-printer.esm.js'
+			)
+		}
+	},
 	plugins: [tailwindcss(), sveltekit()]
 });
